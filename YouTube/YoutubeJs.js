28eefabeb14c09d55c23474383c168ea021eadb7
hanhhg0607 <hanhgtd00593@fpt.edu.vn>
2018-10-30 15:53:27 +0700

    Hello

--- YouTube/YoutubeJs.js
+++ YouTube/YoutubeJs.js
@@ -1,59 +1,35 @@
-var http = new XMLHttpRequest();
-http.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var jsObject = JSON.parse(http.responseText);
-        var content = '';
-        console.log(http.responseText);
-        for (var i = 0; i < jsObject.items.length; i++) {
+var myModal = document.getElementById('myModal');
+var youtubeFrame = document.getElementById('youtubeFrame');
+var span = document.getElementsByClassName("close")[0];
 
-            var itemContent = '<div class="tube-item">';
-            itemContent += '<img id="myImg" width="300" height="auto" src="' + jsObject.items[i].snippet.thumbnails.high.url + '"/>';
-            itemContent += ' <div id="myModal" class="modal">';
-            itemContent += '<span class="close">&times;</span>';
-            itemContent += '<iframe class="modal-content" width="660" height="355" src="https://www.youtube.com/embed/' + jsObject.items[i].id.videoId + '" frameborder="0"  allow="autoplay; encrypted-media" allowfullscreen>';
-            itemContent += '</iframe>';
-            itemContent += ' <div id="caption"></div>';
-            itemContent += '</div>';
-            itemContent += '<h3 style="font-size: 16px">' + jsObject.items[i].snippet.title + '</h3>';
-            itemContent += '</div>';
-            // var itemContent = '<div class="tube-item">';
-            // itemContent += '<iframe width="660" height="355" src="https://www.youtube.com/embed/' + jsObject.items[i].id.videoId + '" frameborder="0"  allow="autoplay; encrypted-media" allowfullscreen>';
-            // itemContent += '</iframe>';
-            // itemContent += '<h3>' + jsObject.items[i].snippet.title + '</h3>';
-            // itemContent += '</div>';
+searchYoutube();
 
-            content += itemContent;
-        }
-        document.getElementById("myTubes").innerHTML = content;
-        var modal = document.getElementById('myModal');
+document.getElementById('btnSearch').onclick = function(){
+    searchYoutube();
+}
 
-        var img = document.getElementById('myImg');
-        var captionText = document.getElementById("caption");
-        img.onclick = function () {
-            modal.style.display = "block";
-            captionText.innerHTML = this.alt;
-        }
-
-        var span = document.getElementsByClassName("close")[0];
-
-        span.onclick = function () {
-            closeVideo();
-
-        }
-
-        function closeVideo() {
-            modal.style.display = "none";
-
-        }
+document.getElementById('keyword').onkeypress = function (event){
+    if (event.keyword != undefined && event.keyword == 13) {
+        searchYoutube();
     }
-};
+}
 
-var submit = document.getElementById("btn-submit");
-submit.onclick = function () {
-    var key = document.getElementById("tim");
-    var keyWord = key.value;
-    var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q=" + keyWord + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
+function doSomething (videoId){
+    youtubeFrame.src = '' + videoId;
+    myModal.style.display = 'block';
+}
 
-    http.open("GET", YOUTUBE_API, true);
-    http.send();
+span.onclick = function(){
+    myModal.style.display = 'none';
+    youtubeFrame.src = '';
+}
+function searchYoutube(){
+    var keyword = document.getElementById('keyword').value;
+    var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q=" 
+        + keyWord 
+        + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        
+    }
 }
